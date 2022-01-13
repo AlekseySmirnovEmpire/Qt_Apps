7efcef4dc4eacd3dba5abf1d4b90a8da373645ae
Aleksey Smirnov <i@smirnov-alexsey.ru>
2022-01-13 12:29:04 +0300

    init

---
+++ 36_2/build/CMakeFiles/Qt_First_App.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Qt_First_App.dir/main.cpp.o"
+  "Qt_First_App"
+  "Qt_First_App.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Qt_First_App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
